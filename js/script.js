9f2140a776a136e4e65dcdca44983be441c587ff
Gabriel Lopes <gabriellopesbusiness@gmail.com>
2020-12-27 23:07:55 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+let addBtn = document.querySelector("#addBtn");
+let taskInput = document.querySelector("#taskInput");
+let todoList = document.querySelector(".todoList");
+let tasks = [];
+
+addBtn.addEventListener("click", () => {
+    tasks.push(buffer());
+    setTask();
+    printTasks();
+    taskInput.value = "";
+});
+
+
+
+function setTask() {
+    localStorage.setItem("todo", JSON.stringify(tasks));
+}
+
+function printTasks() {
+    todoList.innerHTML = "";
+    tasks.forEach((item) => {
+        todoList.innerHTML +=
+            `<div class="task">
+                <input type="checkbox" onclick="finished(${item.id})" ${item.complete == true ? "checked" : ""}>
+                <input type="text" value="${item.text}">
+                <input type="button" onclick="remove(${item.id})" value="Delete">
+            </div>`;
+    });
+}
+
+function finished(i) {
+    tasks.forEach((item) => {
+        if (item.id == i) {
+            item.complete = item.complete ? false : true;
+        }
+    });
+    setTask();
+    printTasks();
+}
+
+//Remove an item from localStorage using index;
+function remove(i) {
+    let buf = [];
+    tasks.forEach((item) => {
+        if (item.id != i) {
+            buf.push(item);
+        }
+    });
+    tasks = buf;
+    setTask();
+    printTasks();
+}
+
+//Get tasks on localStorage and set in tasks
+function getTasks() {
+    if (localStorage.getItem("todo") == null || localStorage.getItem("todo") == "") {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("todo"));
+    }
+}
+
+function buffer() {
+    let buf = {};
+    buf.id = tasks.length;
+    buf.text = taskInput.value;
+    buf.complete = false;
+    return buf;
+}
+
+getTasks();
+printTasks();
